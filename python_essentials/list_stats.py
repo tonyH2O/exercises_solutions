4c9f9fe3b1b26534418360377dbb0f14a9d7b0d4
H2O <44346172+tonyH2O@users.noreply.github.com>
2020-03-29 10:09:30 +0300

    list_statistics
    use lists to print out a statistics

---
+++ python_essentials/list_stats.py
@@ -0,0 +1,46 @@
+'''
+Problem: 
+
+You will be given a number n. On the next n lines you will receive integers. You have to create and print two lists:
+ One with all the positives (including 0)
+ One with all the negatives
+Finally print the following message: 'Count of positives: {count_positives}. Sum of negatives:
+{sum_of_negatives}'
+
+Input:
+
+5
+10
+3
+2
+-15
+-4
+
+Output
+
+[10, 3, 2]
+[-15, -4]
+Count of positives: 3. Sum of negatives: -19
+
+Solution:
+
+'''
+
+n = int(input('n: '))
+
+positive = []
+negative = []
+sums = 0
+
+for x in range(n):
+    b = int(input(': '))
+    if x >= 0 and b >= 0:
+        positive += [b]
+    else:
+        negative += [b]
+        sums += b
+
+print(positive)
+print(negative)
+count = len(positive)
+print('Count of positives: {}. Sum of negatives: {}'.format(count, sums))
